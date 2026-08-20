13b679f93314d00ca04dcc162b53c7a1aa3da8a0
Spencer Smith <spencerliamsmith@gmail.com>
2026-08-20 04:16:23 -0600

    remove out dir

--- out/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod utils;
-
-fn main() {
-    println!("{}", utils::times_three_plus_two(5.0));
-    println!("{}", utils::times_three_plus_two(10.0));
-    println!("{}", utils::times_three_plus_two(-2.0));
-    println!("{}", utils::times_three_plus_two(2.5));
-}
--- out/src/utils.rs
+++
@@ -1,3 +0,0 @@
-pub fn times_three_plus_two(x: f64) -> f64 {
-    3.0 * x + 2.0
-}
